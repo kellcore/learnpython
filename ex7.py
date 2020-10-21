40c90a9200fd34e7dec66c1b7cfdc8757cde0524
kellcore <48416779+kellcore@users.noreply.github.com>
2020-10-21 12:36:15 -0400

    updated comments for ex7.py

--- ex7.py
+++ ex7.py
@@ -1,8 +1,9 @@
-print("Mary had a little lamb.")
+print("Mary had a little lamb.")  # prints line
+# prints formatted line and inserts 'snow'
 print("Its fleece was white as {}.".format('snow'))
-print("And everywhere that Mary went.")
-print("." * 10)  # that prints out ten .s
-
+print("And everywhere that Mary went.")  # prints line
+print("." * 10)  # prints out ten .s
+# saves each letter as a different variable
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -17,5 +18,9 @@ end11 = "e"
 end12 = "r"
 
 # watch end = ' ' at the end. removing it puts Cheese and Burger on two different lines
+# prints Cheese using string concatenation, end=' ' adds a space between the words, prints Burger using string concatenation
 print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
-print(end7 + end8 + end9 + end10 + end11 + end12)
+print(end7 + end8 + end9 + end10 + end11 + end12)  # prints Burger
+
+# can write a string using single or double quotes
+# short strings = ' ' longer strings = " "
